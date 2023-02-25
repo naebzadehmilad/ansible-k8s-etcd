f818354f89b1492313a3b813e49c88421e7bc011
naebzadehmilad <45167308+naebzadehmilad@users.noreply.github.com>
2023-02-25 11:30:44 +0330

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,1 +1,2 @@
 ansible -m shell k8s -a 'echo nameserver 185.51.200.1 > /etc/resolv.conf ; apt upgrade -y kubeadm=1.23.16-00 kubectl=1.23.16-00 kubelet=1.23.16-00' -i inventory.yml
+apt-mark hold kubelet kubeadm kubectl
